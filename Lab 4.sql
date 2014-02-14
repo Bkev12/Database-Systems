774be236b3b939665a69a3ac4af08c79d3fc55e5
Kevin <kevbru94@gmail.com>
2014-02-13 22:50:17 -0500

    Lab 4
    Kevin Bruce
    2/13/14
    Lab 4

---
+++ Lab 4.sql
@@ -0,0 +1,65 @@
+﻿--- Kevin Bruce 
+--- 2/12/14
+--- Lab 4
+
+select city
+from agents 
+where aid in (select aid 
+	       from orders 
+	       where cid in (select cid 
+			     from customers 
+			     where name= 'Basics')
+			     );
+
+select pid 
+from orders 
+where aid in (select aid 
+	       from orders
+	       where cid in (select cid 
+			    from customers 
+		            where city = 'Kyoto' )
+				        );
+				        
+select cid, name 
+from customers 
+where cid in (select cid 
+		from orders 
+		where aid in (select aid 
+				from agents 
+				where aid != 'a03')
+				);
+
+select cid, name 
+from customers 
+where cid in (select cid 
+		from orders 
+		where pid = 'p01'
+		and cid in (select cid 
+			     from orders 
+			     where pid = 'p07')
+			     );
+
+
+select pid 
+from orders 
+where cid in (select cid 
+	       from orders
+	       where aid = 'a03');	
+
+
+select name, discount 
+from customers 
+where cid in (select cid  
+	     from orders 
+	     where aid in (select aid 
+			    from agents 
+			    where city = 'Duluth'
+			    or city = 'Dallas')
+			    );
+
+select *
+from customers 
+where discount in (select discount 
+		from customers
+		where city = 'Dallas'
+		or city = 'Kyoto');
